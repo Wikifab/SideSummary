9eeec34404a77707b970d18942c89141d3e25462
Solene <solene.moins@gmail.com>
2017-11-21 10:04:01 +0100

    Ajout de la width auto au container quand on ouvre le menu

--- resources/wikifab-side-summary.js
+++ resources/wikifab-side-summary.js
@@ -12,6 +12,7 @@
 			$('.vertical-sidebar').css('width', '250px');
 			$('body').addClass("pushBodyLeft");
 			$('body').removeClass("pushBodyRight");
+			$(".container").attr("width","auto");
 			$('.openNav').hide();
 		});
 		$('.closebtn').click(function(){
