d30a05a012ffa846e86157d075d7451debb2a699
Solene <solene.moins@gmail.com>
2017-12-05 13:38:26 +0100

    Retrait des pseudo mediaQueries en js

--- resources/wikifab-side-summary.js
+++ resources/wikifab-side-summary.js
@@ -2,24 +2,14 @@
 (function() {
 	
 	$(document).ready(function() {
-		// Pour les deux cas (SideSummary et PageSummary)
-		
-
-		if (window.matchMedia("(max-width: 600px)").matches) {
-					$('.sidePageSummary').css('width', '100%');
-					$('.SideSummary').css('width', '100%');
-					$('.buttonClose').show();
-					$('.buttonOpen').hide();
-				}
 		
 		// Seulement pour SideSummary
 		if ($('.SideSummary').length > 0){
 			$('body').addClass("HasSideSummary");
-			
+				
 			// Bouton pour ouvrir le menu avec un effet de push sur la droite 
 			$('.buttonOpen').click(function(){
 				$('.buttonClose').show();
-				$('.sidePageSummary').css('width', '250px');
 				$('.SideSummary').css('width', '250px');
 				$('body').addClass("pushBodyLeft");
 				$('body').removeClass("pushBodyRight");
@@ -32,12 +22,12 @@
 			//Bouton pour fermer le menu
 			$('.buttonClose').click(function(){
 				$('.buttonClose').hide();
-				$('.sidePageSummary').css('width', '0px');
 				$('.SideSummary').css('width', '0px');
 				$('body').removeClass("pushBodyLeft");
 				$('body').addClass("pushBodyRight");
 				$('.buttonOpen').show();
 			});
+				
 			// Permet de faire fonctionner le système des flèches avec les sous-menus 
 			$('.vertical-sidebar .selflink').parents(".vertical-sidebar li").addClass('summarycollapse');
 			$('.vertical-sidebar .selflink').parent().addClass('active');
@@ -56,15 +46,7 @@
 			$('.down-arrow').click(function(){
 				$(this).parent().removeClass('summarycollapse');
 			});
-			
-			
-			// Si l'écran est plus grand que 1200px on affiche directement le menu
-			if(window.matchMedia("(min-width: 1200px)").matches){
-				$('.buttonOpen').hide();
-				$('.SideSummary').css('width', '250px');
-				$('body').addClass("pushBodyLeft");
-				$('body').removeClass("pushBodyRight");
-			}
+						
 			$(window).scroll(function() {    
 
 			    var scroll = $(window).scrollTop();
@@ -87,13 +69,7 @@
 			// Bouton pour ouvrir le menu avec un effet de push sur la droite 
 			$('.openNav').click(function(){
 				$('.closebtn').show();
-				if (window.matchMedia("(max-width: 600px)").matches) {
-					$('.sidePageSummary').css('width', '100%');
-					$('.SideSummary').css('width', '100%');
-
-				}
 				$('.sidePageSummary').css('width', '250px');
-				$('.SideSummary').css('width', '250px');
 				$('body').addClass("pushBodyLeft");
 				$('body').removeClass("pushBodyRight");
 				
