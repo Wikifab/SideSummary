ef999935f6a6851b59ef4618bf2cddf87bb6a233
Solene <solene.moins@gmail.com>
2017-12-27 17:01:28 +0100

    Modifications des tailles gérées et des effets

--- resources/wikifab-side-summary.js
+++ resources/wikifab-side-summary.js
@@ -5,50 +5,37 @@
 		
 		// Seulement pour SideSummary
 		if ($('.SideSummary').length > 0){
-			
-			$('.containerBodyWithoutSS').addClass("HasSideSummary");
-			
-			//Quand on est à moins de 600px (portables)                
-			if (window.matchMedia("(max-width: 600px)").matches) {
-				$('.buttonCloseNav').show();
+					
+			//Quand on est à moins de 768px (portables jusqu'à l'ipad)                
+			if (window.matchMedia("(max-width: 768px)").matches) {
+				// Menu fermé de base
+				$('.buttonOpen').show();
+				$('.SideSummary').css('width', '0px');
+				$('.containerBodyWithoutSS').removeClass("positionFixed");
+				$('.containerBodyWithoutSS').removeClass("PushOverlay");
 				
-
-				$('.SideSummary').css('width', '80%');
-				$('.buttonOpen').hide();
-				$('.containerBodyWithoutSS').addClass("positionFixed");
-				//$('#PushOverlay').addClass("active");
 				// Bouton pour ouvrir le menu avec un effet de push sur la droite 
 				$('.buttonOpen').click(function(){
 					$('.SideSummary').css('width', '80%');
 					$('.buttonOpen').hide();
 					$('.containerBodyWithoutSS').addClass("positionFixed");
-					$('.containerBodyWithoutSS').removeClass("pushBodyLeft");
-					//$('#PushOverlay').addClass("active");
-					$('.buttonCloseNav').show();
+					$('.containerBodyWithoutSS').addClass("PushOverlay");
+
+					
 				});
 				
-				$('.buttonCloseNav').click(function(){
+				$('.containerBodyWithoutSS').click(function(){
 					$('.buttonOpen').show();
 					$('.SideSummary').css('width', '0px');
 					$('.containerBodyWithoutSS').removeClass("positionFixed");
-					$('.containerBodyWithoutSS').removeClass("pushBodyLeft");
-					$('.containerBodyWithoutSS').removeClass("pushBodyRight");
-					$('.buttonCloseNav').hide();
+					$('.containerBodyWithoutSS').removeClass("PushOverlay");
 
-				});
-						
-				// Ajouter une interaction fermer le menu sur la zone grisée (portables)
-				$('#PushOverlay').click(function(){ // Ou qu'on appuie sur le boutonCloseNav !!!
-					$('.buttonCloseNav').hide();
-					$('.SideSummary').css('width', '0px');
-					$('.buttonOpen').show();
-					$('#PushOverlay').removeClass("active");
-				});
-								
+
+				});							
 			}
 			
 				
-			if(window.matchMedia("(min-width: 1200px)").matches){
+			if(window.matchMedia("(min-width: 769px)").matches){
 				
 				$('.buttonClose').show();
 				$('.buttonOpen').hide();
@@ -86,46 +73,28 @@
 					}
 				});
 				
-			}
-		
-		else {
-//			// De base le menu est fermé 
-//			$('.buttonClose').click(function(){
-//			$('.buttonOpen').show();
-//			$('.SideSummary').css('width', '0px');
-//			$('.containerBodyWithoutSS').addClass("pushBodyRight");
-//			$('.containerBodyWithoutSS').removeClass("pushBodyLeft");
-//			
-//			});
-//		
-//			$('.buttonOpen').click(function(){
-//				$('.buttonClose').show();
-//				$('.buttonOpen').hide();
-//				$('.SideSummary').css('width', '250px');
-//				$('.containerBodyWithoutSS').addClass("pushBodyLeft");
-//				$('.containerBodyWithoutSS').removeClass("pushBodyRight");
-//			});
-//				
-		}
-			
-		// Permet de faire fonctionner le système des flèches avec les sous-menus (avec tous les tailles d'écran)
-		$('.vertical-sidebar .selflink').parents(".vertical-sidebar li").addClass('summarycollapse');
-		$('.vertical-sidebar .selflink').parent().addClass('active');
-		
-		$( '.vertical-sidebar li ' )
-			.filter(function( index ) {
-				return $( "li", this ).length > 0 ;
-			})
-		    .prepend("<i class=\"right-arrow fa fa-chevron-right\" aria-hidden=\"true\"></i>")
-		    .prepend("<i class=\"down-arrow fa fa-chevron-down\" aria-hidden=\"true\"></i>");
+				$('.vertical-sidebar .selflink').parents(".vertical-sidebar li").addClass('summarycollapse');
+				$('.vertical-sidebar .selflink').parent().addClass('active');
+				
+				$( '.vertical-sidebar li ' )
+					.filter(function( index ) {
+						return $( "li", this ).length > 0 ;
+					})
+				    .prepend("<i class=\"right-arrow fa fa-chevron-right\" aria-hidden=\"true\"></i>")
+				    .prepend("<i class=\"down-arrow fa fa-chevron-down\" aria-hidden=\"true\"></i>");
 
+				
+				$('.right-arrow').click(function(){
+					$(this).parent().addClass('summarycollapse');
+				});
+				$('.down-arrow').click(function(){
+					$(this).parent().removeClass('summarycollapse');
+				});
+				
+			}
+					
+		// Permet de faire fonctionner le système des flèches avec les sous-menus (avec tous les tailles d'écran)
 		
-		$('.right-arrow').click(function(){
-			$(this).parent().addClass('summarycollapse');
-		});
-		$('.down-arrow').click(function(){
-			$(this).parent().removeClass('summarycollapse');
-		});
 	}
 
 	});		
