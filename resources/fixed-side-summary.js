4ce5217fd3b14f18c89c4023975a7da0a9cab390
Pierre Boutet <boutetpierre@yahoo.fr>
2018-02-15 12:44:49 +0100

    fix menu position

--- resources/fixed-side-summary.js
+++ resources/fixed-side-summary.js
@@ -4,36 +4,27 @@ $(document).ready(function() {
 
 	var initialPosition = 135;
 
-	var H = $(document).height();
-	var h = 150 ;
-
 	function refreshPosition() {
 
-        //console.log('ss-fixed-top ' + $(document).height() + ':' + H + ' ' + $(document).height() +':' + h + ' ' + (H -h) + ' > ' +$(window).scrollTop() + ' ' + initialPosition);
-
-		if ($(window).scrollTop() > (H -h)) {
-	        $('.fixedSideSummary').css('top',(H -h));
-	        $('.fixedSideSummary').removeClass('ss-fixed-top');
-	        $('.fixedSideSummary').addClass('ss-fixed-bottom');
-	        $('.fixedSideSummary').removeClass('ss-unfixed');
-		} else if ($(window).scrollTop() > initialPosition) {
-	        $('.fixedSideSummary').css('top',"");
-	        $('.fixedSideSummary').addClass('ss-fixed-top');
-	        $('.fixedSideSummary').removeClass('ss-fixed-bottom');
-	        $('.fixedSideSummary').removeClass('ss-unfixed');
-	    } else {
-	        $('.fixedSideSummary').css('top',"");
-	        $('.fixedSideSummary').removeClass('ss-fixed-top');
-	        $('.fixedSideSummary').removeClass('ss-fixed-bottom');
-	        $('.fixedSideSummary').addClass('ss-unfixed');
-	    }
+		if ($(window).scrollTop() > initialPosition) {
+	        $('.fixedSideSummary').css('height',"100%");
+	        $('.fixedSideSummary').css('overflow',"auto");
+			$('.fixedSideSummary').addClass('ss-fixed-top');
+			$('.fixedSideSummary').removeClass('ss-fixed-bottom');
+			$('.fixedSideSummary').removeClass('ss-unfixed');
+		} else {
+	        $('.fixedSideSummary').css('height',"");
+	        $('.fixedSideSummary').css('overflow',"");
+			$('.fixedSideSummary').removeClass('ss-fixed-top');
+			$('.fixedSideSummary').removeClass('ss-fixed-bottom');
+			$('.fixedSideSummary').addClass('ss-unfixed');
+		}
 	}
 
 	if( $('.fixedSideSummary').length) {
 
-		H = $(document).height();
-		h = $('.fixedSideSummary').height() ;
-
+		$('.fixedSideSummary').css('clear','both') ;
+		
         $('.fixedSideSummary').addClass('ss-unfixed');
 
 		initialPosition = $('.fixedSideSummary').offset().top;
