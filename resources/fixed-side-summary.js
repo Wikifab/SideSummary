1ec203e1040643b4478490032940deefd5c6c38f
Pierre Boutet <boutetpierre@yahoo.fr>
2018-02-15 14:02:10 +0100

    add footer management on fixing menu

--- resources/fixed-side-summary.js
+++ resources/fixed-side-summary.js
@@ -4,17 +4,37 @@ $(document).ready(function() {
 
 	var initialPosition = 135;
 
-	function refreshPosition() {
+	var H = $(document).height();
+	var h = 150 ;
 
-		if ($(window).scrollTop() > initialPosition) {
-	        $('.fixedSideSummary').css('height',"100%");
-	        $('.fixedSideSummary').css('overflow',"auto");
+	function refreshPosition() {
+		
+		var wh = $(window).height();
+		var footerPos = null;
+		if ($('.footer-main').length) {
+			footerPos = $('.footer-main').offset().top;
+		}
+		
+		if (footerPos && $(window).scrollTop() + wh > footerPos) {
+			var pos = footerPos - $(window).scrollTop() - wh;
+			$('.fixedSideSummary').css('top',pos);
+			$('.fixedSideSummary').css('height',"100%");
+			$('.fixedSideSummary').css('overflow',"auto");
+			$('.fixedSideSummary').addClass('ss-fixed-top');
+			$('.fixedSideSummary').removeClass('ss-fixed-bottom');
+			$('.fixedSideSummary').removeClass('ss-unfixed');
+			
+		} else if ($(window).scrollTop() > initialPosition) {
+			$('.fixedSideSummary').css('top',"");
+			$('.fixedSideSummary').css('height',"100%");
+			$('.fixedSideSummary').css('overflow',"auto");
 			$('.fixedSideSummary').addClass('ss-fixed-top');
 			$('.fixedSideSummary').removeClass('ss-fixed-bottom');
 			$('.fixedSideSummary').removeClass('ss-unfixed');
 		} else {
-	        $('.fixedSideSummary').css('height',"");
-	        $('.fixedSideSummary').css('overflow',"");
+			$('.fixedSideSummary').css('top',"");
+			$('.fixedSideSummary').css('height',"");
+			$('.fixedSideSummary').css('overflow',"");
 			$('.fixedSideSummary').removeClass('ss-fixed-top');
 			$('.fixedSideSummary').removeClass('ss-fixed-bottom');
 			$('.fixedSideSummary').addClass('ss-unfixed');
@@ -24,15 +44,16 @@ $(document).ready(function() {
 	if( $('.fixedSideSummary').length) {
 
 		$('.fixedSideSummary').css('clear','both') ;
-		
-        $('.fixedSideSummary').addClass('ss-unfixed');
+		h = $('.fixedSideSummary').height() ;
+
+		$('.fixedSideSummary').addClass('ss-unfixed');
 
 		initialPosition = $('.fixedSideSummary').offset().top;
 
-    	$(window).scroll(function(){
-    		refreshPosition();
-    	});
-    	refreshPosition();
+		$(window).scroll(function(){
+			refreshPosition();
+		});
+		refreshPosition();
 	}
 
 	$('body').mwscrollspy({target: "#toc"});
